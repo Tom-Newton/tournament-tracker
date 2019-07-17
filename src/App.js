151f622b18c2274a6760a404917f39a81ce899b4
Thomas Newton <thomas.w.newton@gmail.com>
2019-07-17 18:44:28 +0100

    Add player entry field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import $ from 'jquery';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +10,15 @@ class App extends React.Component {
         tabsData: [
           {
             tabName: "Player Input",
-            renderTabContent: () => <PlayerInput></PlayerInput>,
+            renderTabContent: (() => {
+              return (
+                <PlayerInput
+                  players={this.state.players}
+                  onChange={(players) => this.handleChange(players)}
+                >
+                </PlayerInput>
+              );
+            }),
           }, {
             tabName: "Game Input",
             renderTabContent: () => <PlayerInput></PlayerInput>,
@@ -23,11 +32,16 @@ class App extends React.Component {
         ],
         activeTabIndex: 0,
       },
-      players: [],
+      players: ["player0", "player1", ""],
     };
   }
 
+  handleChange(players) {
+    this.setState({players: players});
+  }
+
   handleClick(index) {
+    // TODO make this immutable
     this.setState((previousState, props) => {
       return previousState.tabs.activeTabIndex = index;
     })
@@ -48,12 +62,84 @@ class App extends React.Component {
 
 export default App;
 
-function PlayerInput(props) {
-  console.log("render PlayerInputContent")
-  const playersList = null;
-  return (
-    <ol>{playersList}</ol>
-  );
+class PlayerInput extends React.Component {
+  focus (index) {
+    return true;
+  }
+
+  editPlayers (value, index) {
+    const players = this.props.players.slice();
+    if (value || this.focus(index)) {
+      players[index] = value;
+    } else {
+      players.splice(index, 1);
+    }
+    if (players[players.length - 1]) {
+      players.push("")
+    }
+    return players;
+  }
+
+  render () {
+    console.log("render PlayerInputContent")
+    // TODO: Probably shouldn't use index for the key here so rerendering is efficient
+    const playerEntries = this.props.players.map((player, index) => {
+      return (
+        <PlayerEntry
+          key={index}
+          id={`inputField${index}`}
+          player={player}
+          onChange={(value) => this.props.onChange(this.editPlayers(value, index))}
+          focus={() => this.focus(index)}
+        >
+        </PlayerEntry>
+      );
+    })
+    return (
+      <ol>
+        {playerEntries}
+      </ol>
+    );
+  }
+}
+
+class PlayerEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      edited: false,
+    }
+  }
+
+  getValue (event) {
+    // this.setState({edited: true});
+    if (!(event.target.id === this.props.id)) {
+      console.log("delete empty")
+    }
+    console.log(event.target)
+    return event.target.value
+  }
+
+  focus () {
+    console.log(document.activeElement)
+  }
+
+  render () {
+    return (
+      <li
+        className="playerEntry"
+      >
+        <input
+          id={this.props.id}
+          type="text"
+          value={this.props.player}
+          placeholder={this.state.edited ? "" : "Click to add new player"}
+          onChange={(event) => this.props.onChange(this.getValue(event))}
+        >
+        </input>
+      </li>
+    );
+  }
 }
 
 function Tabs(props) {
