dde5f7dcc9f70b223d34555bf01eae262c6d60ff
Thomas Newton <thomas.w.newton@gmail.com>
2019-07-18 20:41:29 +0100

    Attempt to add advanced features to player input fields

--- src/App.js
+++ src/App.js
@@ -63,34 +63,85 @@ class App extends React.Component {
 export default App;
 
 class PlayerInput extends React.Component {
-  focus (index) {
-    return true;
+  constructor(props) {
+    super(props);
+    this.state = {
+      players: this.props.players,
+    };
+  }
+  // TODO: stop double rerenders due to handleChange and handleKeyDown
+  async handleChange (value, index) {
+    console.log("handle change")
+    this.setState((state) => {
+      const players = state.players.slice();
+      console.log(value)
+      players[index] = value;
+      if (players[players.length - 1]) {
+        players.push("")
+      }
+      return state.players = players;
+    });
   }
 
-  editPlayers (value, index) {
-    const players = this.props.players.slice();
-    if (value || this.focus(index)) {
-      players[index] = value;
-    } else {
-      players.splice(index, 1);
+  async handleKeyDown (keyCode, index) {
+    console.log("handlekeydown")
+    this.setState((state) => {
+      const players = state.players.slice();
+      if (keyCode === 8 || keyCode === 13) {
+        if (keyCode === 8) {
+          // Backspace
+          if (!players[index] && index < players.length - 1) {
+            // Field is empty -> delete field and focus above
+            players.splice(index, 1);
+            // TODO: Stop this from backspacing on the line above
+            // document.getElementById(`inputField${index - 1}`).focus()
+          }
+        } else {
+          if (players[index]) {
+            if (players[index + 1]) {
+              // Next fieldis not empty -> add an empty field below
+              players.splice(index + 1, 0, "")
+            }
+            if (index < players.length - 1) {
+              // Focus field below
+              document.getElementById(`inputField${index + 1}`).focus()
+              console.log("Focused next line")
+            }
+          }
+        }
+      }
+        return state.players = players;
+      });
+
     }
-    if (players[players.length - 1]) {
-      players.push("")
-    }
-    return players;
+
+  async handleBlur (index) {
+    console.log("handleBlur")
+    this.setState((state) => {
+      const players = this.state.players.slice();
+      if (!players[index] && index < players.length - 1) {
+        // Clicked off an empty field -> delete that field
+        players.splice(index, 1);
+        console.log("deleted line: " + index)
+        this.setState((state) => {
+          return state.players = players;
+        });
+      }
+    });
   }
 
   render () {
-    console.log("render PlayerInputContent")
     // TODO: Probably shouldn't use index for the key here so rerendering is efficient
-    const playerEntries = this.props.players.map((player, index) => {
+    const playerEntries = this.state.players.map((player, index) => {
       return (
         <PlayerEntry
           key={index}
           id={`inputField${index}`}
           player={player}
-          onChange={(value) => this.props.onChange(this.editPlayers(value, index))}
-          focus={() => this.focus(index)}
+          focus={index === 1}
+          onChange={(value) => this.handleChange(value, index)}
+          onKeyDown={(keyCode) => this.handleKeyDown(keyCode, index)}
+          onBlur={() => this.handleBlur(index)}
         >
         </PlayerEntry>
       );
@@ -104,27 +155,11 @@ class PlayerInput extends React.Component {
 }
 
 class PlayerEntry extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      edited: false,
-    }
-  }
-
-  getValue (event) {
-    // this.setState({edited: true});
-    if (!(event.target.id === this.props.id)) {
-      console.log("delete empty")
-    }
-    console.log(event.target)
-    return event.target.value
-  }
-
-  focus () {
-    console.log(document.activeElement)
-  }
-
   render () {
+    if (this.props.focus) {
+      console.log("focus");
+      this.focusTextInput();
+    }
     return (
       <li
         className="playerEntry"
@@ -133,8 +168,10 @@ class PlayerEntry extends React.Component {
           id={this.props.id}
           type="text"
           value={this.props.player}
-          placeholder={this.state.edited ? "" : "Click to add new player"}
-          onChange={(event) => this.props.onChange(this.getValue(event))}
+          placeholder={"Click to add new player"}
+          onChange={(event) => this.props.onChange(event.target.value)}
+          onKeyDown={(event) => this.props.onKeyDown(event.keyCode)}
+          onBlur={() => this.props.onBlur()}
         >
         </input>
       </li>
