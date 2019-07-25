e33ef52a2b1d1b3d2aa82282901f4d3bf5928b4b
Thomas Newton <thomas.w.newton@gmail.com>
2019-07-25 13:38:55 +0300

    Remember state from previous session

--- src/App.js
+++ src/App.js
@@ -6,54 +6,65 @@ import GameInput from './GameInput.js';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      tabs: {
-        tabsData: [
-          {
-            tabName: "Player Input",
-            renderTabContent: (() => {
-              return (
-                <PlayerInput
-                  players={this.state.players}
-                  onChange={(players) => this.handleChange("players", players)}
-                >
-                </PlayerInput>
-              );
-            }),
-          }, {
-            tabName: "Game Input",
-            renderTabContent: () => {
-              return (
-                <GameInput
-                  games={this.state.games}
-                  players={this.state.players}
-                  onChange={(games) => this.handleChange("games", games)}
-                >
-                </GameInput>
-              );
-            },
-          }, {
-            tabName: "Results Input",
-            renderTabContent: () => <div>Results input</div>,
-          }, {
-            tabName: "Results Table",
-            renderTabContent: () => <div>results table</div>,
-          },
-        ],
-        activeTabIndex: 1,
-      },
-      players: ["player0", "player1", "player2"],
-      games: [{
-        gameName: "gamename",
-        includedPlayers: ["player0", "player1", "player2"],
-        teams: {
-          numberOfTeams: 2,
-          teamsData: [],
+    const tabsData = [
+      {
+        tabName: "Player Input",
+        renderTabContent: (() => {
+          return (
+            <PlayerInput
+            players={this.state.players}
+            onChange={(players) => this.handleChange("players", players)}
+            >
+            </PlayerInput>
+          );
+        }),
+      }, {
+        tabName: "Game Input",
+        renderTabContent: () => {
+          return (
+            <GameInput
+            games={this.state.games}
+            players={this.state.players}
+            onChange={(games) => this.handleChange("games", games)}
+            >
+            </GameInput>
+          );
         },
-        gameData: [],
-      }],
+      }, {
+        tabName: "Results Input",
+        renderTabContent: () => <div>Results input</div>,
+      }, {
+        tabName: "Results Table",
+        renderTabContent: () => <div>results table</div>,
+      },
+    ]
+
+    const defaultState = {
+      tabs: {
+        activeTabIndex: 0,
+      },
+      players: [],
+      games: [],
     };
+
+    const storedState = JSON.parse(localStorage.getItem("storedState"));
+    // Use defaultState unless a state is stored in localstorage
+    let state;
+    if (storedState) {
+      console.log("loaded state")
+      state = storedState;
+    } else {
+      state = defaultState;
+    }
+
+    // Add in tabsData which hould never change
+    state.tabs.tabsData = tabsData;
+    this.state = state;
   }
+
+  componentDidUpdate () {
+    localStorage.setItem("storedState", JSON.stringify(this.state));
+  };
 
   handleChange(area, object) {
     switch (area) {
