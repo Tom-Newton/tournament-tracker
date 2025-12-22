19a8bb7f84f67afe97ac0291733f89a01e41530f
Thomas Newton <thomas.w.newton@gmail.com>
2025-12-22 14:06:05 +0000

    Fix results table issue

--- src/getTeam.js
+++ src/getTeam.js
@@ -2,10 +2,13 @@ export default function getTeam(teamReference, gameData) {
   if (teamReference.sourceType === "number") {
     return teamReference;
   } else if (teamReference.sourceType === "rank") {
-    teamReference =
-      gameData[teamReference.sourceRound].roundData[
-        teamReference.sourceSubRound
-      ].subRoundData.leaderboard[teamReference.rank].teamReference;
+    if (teamReference.rank == null) {
+      return null
+    }
+    const subRoundData = gameData[teamReference.sourceRound].roundData[
+      teamReference.sourceSubRound
+    ].subRoundData
+    teamReference = subRoundData.leaderboard[teamReference.rank].teamReference;
     return getTeam(teamReference, gameData);
   }
 }
